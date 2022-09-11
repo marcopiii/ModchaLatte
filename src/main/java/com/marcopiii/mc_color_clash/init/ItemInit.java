06f594964acb47dd9a1ecbb5334888a1956f4819
marcopiii <marpi93@icloud.com>
2022-09-12 01:16:18 +0200

    add rabbit iron `ArmorItem`s

--- src/main/java/com/marcopiii/mc_color_clash/init/ItemInit.java
+++ src/main/java/com/marcopiii/mc_color_clash/init/ItemInit.java
@@ -1,6 +1,10 @@
 package com.marcopiii.mc_color_clash.init;
 
 import com.marcopiii.mc_color_clash.ColorClashMod;
+import com.marcopiii.mc_color_clash.base.RabbitIronArmorMaterial;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,9 +18,33 @@ public class ItemInit {
             "rabbit_iron_nugget",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
     );
-
     public static final RegistryObject<Item> RABBIT_IRON_INGOT = ITEMS.register(
             "rabbit_iron_ingot",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
     );
+
+    private static final Item.Properties rabbitArmorProps = new Item.Properties()
+            .setNoRepair()
+            .tab(CreativeModeTab.TAB_COMBAT);
+
+    public static final RegistryObject<ArmorItem> RABBIT_IRON_HELMET = ITEMS.register(
+            "rabbit_iron_helmet",
+            () -> new ArmorItem(ArmorTiers.RABBIT_IRON_ARMOR_MATERIAL, EquipmentSlot.HEAD, rabbitArmorProps)
+    );
+    public static final RegistryObject<ArmorItem> RABBIT_IRON_CHESTPLATE = ITEMS.register(
+            "rabbit_iron_chestplate",
+            () -> new ArmorItem(ArmorTiers.RABBIT_IRON_ARMOR_MATERIAL, EquipmentSlot.CHEST, rabbitArmorProps)
+    );
+    public static final RegistryObject<ArmorItem> RABBIT_IRON_LEGGINGS = ITEMS.register(
+            "rabbit_iron_leggings",
+            () -> new ArmorItem(ArmorTiers.RABBIT_IRON_ARMOR_MATERIAL, EquipmentSlot.LEGS, rabbitArmorProps)
+    );
+    public static final RegistryObject<ArmorItem> RABBIT_IRON_BOOTS = ITEMS.register(
+            "rabbit_iron_boots",
+            () -> new ArmorItem(ArmorTiers.RABBIT_IRON_ARMOR_MATERIAL, EquipmentSlot.FEET, rabbitArmorProps)
+    );
+
+    public static class ArmorTiers {
+        public static final ArmorMaterial RABBIT_IRON_ARMOR_MATERIAL = new RabbitIronArmorMaterial();
+    }
 }
