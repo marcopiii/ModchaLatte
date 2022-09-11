8a190f4c26f763b1ece02f6ae615cb1727477b57
marcopiii <marpi93@icloud.com>
2022-09-11 11:33:49 +0200

    setup `mods.toml` and base `@Mod`

---
+++ src/main/java/com/marcopiii/mc_color_clash/ColorClashMod.java
@@ -0,0 +1,10 @@
+package com.marcopiii.mc_color_clash;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ColorClashMod.MOD_ID)
+public class ColorClashMod {
+
+    public static final String MOD_ID = "mc_color_clash";
+
+}
