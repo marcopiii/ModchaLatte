eb96ae6e80834a6cf475b2e09078be3038db4b24
marcopiii <marpi93@icloud.com>
2022-09-12 20:05:55 +0200

    Add `EnchantmentCategory` for `ColorBlessingEnchantment`

--- src/main/java/com/marcopiii/mc_color_clash/enchantments/ColorBlessingEnchantment.java
+++ src/main/java/com/marcopiii/mc_color_clash/enchantments/ColorBlessingEnchantment.java
@@ -1,17 +1,39 @@
 package com.marcopiii.mc_color_clash.enchantments;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import org.jetbrains.annotations.NotNull;
+import java.util.Arrays;
 
 public class ColorBlessingEnchantment extends Enchantment {
     private final ColorBlessing color;
 
+    static EnchantmentCategory COLOR_BLESSING_ENCHANTMENT_CATEGORY = EnchantmentCategory.create(
+            "color_blessing",
+            item -> {
+                if (item instanceof ArmorItem) {
+                    EquipmentSlot slot = ((ArmorItem) item).getSlot();
+                    return Arrays.asList(
+                            EquipmentSlot.HEAD,
+                            EquipmentSlot.CHEST,
+                            EquipmentSlot.LEGS,
+                            EquipmentSlot.FEET
+                    ).contains(slot);
+                } else {
+                    return item instanceof SwordItem
+                        || item instanceof AxeItem
+                        || item instanceof CrossbowItem
+                        || item instanceof BowItem;
+                }
+            }
+    );
+
     public ColorBlessingEnchantment(ColorBlessing color) {
         super(
                 Rarity.VERY_RARE,
-                EnchantmentCategory.ARMOR,
+                COLOR_BLESSING_ENCHANTMENT_CATEGORY,
                 new EquipmentSlot[] {
                         EquipmentSlot.HEAD,
                         EquipmentSlot.LEGS,
